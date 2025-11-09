829ad9baeed30cdbec7e465f39c0e317099ab400
Aastik Mehta <mehtaaastik12@gmail.com>
2025-11-10 00:33:46 +0530

    this is not the thread safe code :: Problem in case of multithreading

--- main.cpp
+++ main.cpp
@@ -1,15 +1,28 @@
 #include<iostream>
 #include"logger.h"
+#include<thread>
 
+void user1logs()
+{
+    Logger* logger1 = Logger::getLogger();
+    logger1->Log("this msg is from user 1");
+}
+
+void user2logs()
+{
+    Logger* logger2 = Logger::getLogger();
+    logger2->Log("this msg is from user 2");
+}
 
 using namespace std;
 
 int main()
 {
-    Logger* logger1 = Logger::getLogger();
-    Logger* logger2 = Logger::getLogger();
+    thread t1(user1logs);
+    thread t2(user2logs);
     
-    logger1->Log("this msg is from user 1");
-    logger2->Log("this msg is from user 2");
+    t1.join();
+    t2.join();
+
     return 0;
 }
