708c6aa56bf3891e3af62bbd6093fe97378281ab
Aastik Mehta <mehtaaastik12@gmail.com>
2025-11-10 00:52:45 +0530

    Delete the copy constructor and copy assignment operator

--- logger.h
+++ logger.h
@@ -11,6 +11,8 @@ static mutex mtx;
 Logger();
 
 public:
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
     static Logger* getLogger();
     void Log(string msg);
 };
